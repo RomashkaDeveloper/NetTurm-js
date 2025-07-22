ce6155807e271f2b055ba3d648f7d157ece9c60c
RomashkaDeveloper <forzaerfahren@outlook.com>
2025-07-22 18:00:59 +0300

    local network link added

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ function createWindow() {
     // Load the url
     (async () => {
         const urls = [
+            "http://192.168.1.21:3000",
             "http://192.168.191.244:3000",
             "https://humane-mistakenly-shepherd.ngrok-free.app/"
         ];
