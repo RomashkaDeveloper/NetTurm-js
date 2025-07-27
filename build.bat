6be64fcdbefacbb0654968a3ae3c830ec1925f22
RomashkaDeveloper <forzaerfahren@outlook.com>
2025-07-27 12:08:54 +0300

    README.md updated

--- build.bat
+++ build.bat
@@ -1,2 +1,17 @@
+@echo off
+echo Установка зависимостей...
 npm install
+IF %ERRORLEVEL% NEQ 0 (
+    echo Ошибка при установке зависимостей.
+    exit /b %ERRORLEVEL%
+)
+
+echo Сборка проекта...
 npm run build
+IF %ERRORLEVEL% NEQ 0 (
+    echo Ошибка при сборке проекта.
+    exit /b %ERRORLEVEL%
+)
+
+echo Готово!
+pause
